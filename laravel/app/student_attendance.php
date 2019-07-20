1d07b62a2cc64fb91e1460269cebf8d4434b8a60
miteshgoplani <miteshgoplani@gmail.com>
2019-07-20 21:19:42 +0530

    Get and Post API added

---
+++ laravel/app/student_attendance.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class student_attendance extends Model
+{
+    protected $table = 'student_attendance';
+    public $timestamps =true;
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/allStudents', 'ApiController@allStudents');
+
+Route::post('/addAttendance', 'ApiController@addAttendance');
+
